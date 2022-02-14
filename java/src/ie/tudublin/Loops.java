5e6663b136481a60c763ad1f070ca0c3d83d2009
Rada1242 <d19125768@mytudublin.ie>
2022-02-14 12:08:15 +0000

    case 4

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -52,7 +52,7 @@ public class Loops extends PApplet
 				rect(cx, cy, rectWidth, rectHeight);			
 				break;
 			case 1:
-				
+			{
 				cx = 0;
 				cy = 0;
 				int red;
@@ -71,6 +71,66 @@ public class Loops extends PApplet
 					
 				}
 				break;
+			}
+			case 3:
+			{
+				
+				/*
+					background(0);
+					int bars = (int) (mouseX / 20.0f);
+					float w = width/ (float)bars;
+					for(int i = 0; i < bars; i++)
+					{
+						noStroke();
+						fill(map(i, 0, bars, 0 , 255), 255, 255);
+						rect(map(i, 0, bars, 0, 500), 0 , w, height);
+					}
+				
+				*/cx = 0;
+				cy = 0;
+				int red;
+				int green;
+				int blue;
+
+				rectMode(CORNER);
+				noStroke();
+				fill(50, 255,255);
+				for ( red = 0; red < 256; red+=25) {
+					for ( green = 0; green < 256; green+=25)
+						for ( blue = 0; blue < 256; blue+=25)
+							fill(red, green, blue);
+					rect(cx, cy, width/10 , height/10);
+					cx += 50;
+					cy += 50;
+					
+				}
+			}
+			case 4:
+			{
+				/*background(0);
+					int bars = (int) (mouseX / 20.0f);
+					float w = width/ (float)bars;
+					for(int i = 0; i < bars; i++)
+					{
+						noStroke();
+						fill(map(i, 0, bars, 0 , 255), 255, 255);
+						rect(map(i, 0, bars, 0, 500), 0 , w, height);
+					}
+					*/
+					background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width/ (float) squares;
+				for(int i = 0; i < squares; i++)
+				{
+					noStroke();
+						fill(map(i, 0, squares, 0 , 255), 255, 255);
+						float x = map(i, 0, squares, 0, width);
+						rect(x, x , h, h);
+						rect((width-h)-x, x, h, h);
+				}
+				break;
+			}
+
 		}
 	}
 
