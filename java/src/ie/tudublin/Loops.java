2cac2e0cb654358638997e81bd4facb3a88d7c17
Rada1242 <d19125768@mytudublin.ie>
2022-02-14 11:44:29 +0000

    hh

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -52,45 +52,8 @@ public class Loops extends PApplet
 				rect(cx, cy, rectWidth, rectHeight);			
 				break;
 			case 1:
-				float h = 500;
-				float w = 250;
-				cx = 0;
-				cy = 0;
-				rectMode(CORNER);
 				
-				
-
-				if (mouseX > w)
-				{
-					cx = w;
-				}
-				else
-				{
-					cx = 0;
-				}
-				if(mouseY < 200 + (h / 2))
-				{
-					h = 250;
-					cy = 0;
-				}
-				if(mouseY > 200 + (h / 2))
-				{
-					cy = 250;
-					h = 250;
-				}
-
-				rect(cx, cy, w, h);
-				/*
-				if (mouseX > cx - (w / 2) && mouseX < cx + (w / 2) && mouseX > (w/2) && mouseX + (w/2) < 500)
-				{	
-					cx = mouseX;
-				}
-				if (mouseY < (cy + (h / 2))/2){
-					h = 250;
-					cy = 125;
-				}
-				rect(cx , cy, w, h);
-				*/
+			
 				break;
 		}
 	}
